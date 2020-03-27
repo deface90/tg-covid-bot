630f0f45e332838c7abf4cb214261ab705cf0f0e
deface90 <deface06@yandex.ru>
2020-03-27 14:08:46 +0500

    init comit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/deface90/tg-covid-bot
+
+go 1.14
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.1
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/jinzhu/configor v1.1.1
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+    "log"
+    "net/http"
+
+    "github.com/PuerkitoBio/goquery"
+    "github.com/go-telegram-bot-api/telegram-bot-api"
+    "github.com/jinzhu/configor"
+)
+
+type Config struct {
+    Token     string
+    SourceUrl string
+}
+
+func main() {
+    var config Config
+    err := configor.Load(&config, "config.json")
+    if err != nil {
+        log.Printf("[ERROR] Failed to read config.json: %v", err)
+    }
+
+    bot, err := tgbotapi.NewBotAPI(config.Token)
+    if err != nil {
+        log.Panic(err)
+    }
+
+    bot.Debug = true
+
+    log.Printf("Authorized on account %s", bot.Self.UserName)
+
+    u := tgbotapi.NewUpdate(0)
+    u.Timeout = 60
+
+    updates, err := bot.GetUpdatesChan(u)
+
+    for update := range updates {
+        if update.Message == nil { // ignore any non-Message Updates
+            continue
+        }
+
+        log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+        var answer string
+        if update.Message.Text == "/start" {
+            answer = "Welcome to Covid19 news bot!\nType any message to get stat\nCreated by deface (t.me/deface90)"
+        }
+
+        res, err := http.Get(config.SourceUrl)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        if res.StatusCode != 200 {
+            log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+        }
+
+        // Load the HTML document
+        doc, err := goquery.NewDocumentFromReader(res.Body)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        doc.Find("#maincounter-wrap").Each(func(i int, s *goquery.Selection) {
+            h := s.Find("h1").Text()
+            if h == "Coronavirus Cases:" {
+                answer += "Coronavirus Cases: " + s.Find("span").Text()
+            }
+            if h == "Recovered:" {
+                answer += "\nRecovered: " + s.Find("span").Text()
+            }
+            if h == "Deaths:" {
+                answer += "\nDeaths: " + s.Find("span").Text()
+            }
+        })
+
+        _ = res.Body.Close()
+
+        msg := tgbotapi.NewMessage(update.Message.Chat.ID, answer)
+
+        _, err = bot.Send(msg)
+        if err != nil {
+            log.Printf("[ERROR] Failed to send message: %v", err)
+        }
+    }
+}
